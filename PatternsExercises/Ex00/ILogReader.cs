7cba9198633960f4f7a15a7bfb7a59dec6a65dc4
benjitshindele7 <144945838+benjitshindele7@users.noreply.github.com>
2025-05-14 08:53:57 +0100

    Update ILogReader.cs

--- PatternsExercises/Ex00/ILogReader.cs
+++ PatternsExercises/Ex00/ILogReader.cs
@@ -1,7 +1,23 @@
-﻿namespace Patterns.Ex00
+using Patterns.Ex00.ExternalLibs;
+
+namespace Patterns.Ex00
 {
-    public interface ILogReader
+    public class FtpLogReader : ILogReader
     {
-        string ReadLogFile(string identificator);
+        private readonly string _login;
+        private readonly string _password;
+        private readonly FtpClient _ftpClient;
+
+        public FtpLogReader(string login, string password)
+        {
+            _login = login;
+            _password = password;
+            _ftpClient = new FtpClient();
+        }
+
+        public string ReadLogFile(string filePath)
+        {
+            return _ftpClient.ReadFile(_login, _password, filePath);
+        }
     }
 }
